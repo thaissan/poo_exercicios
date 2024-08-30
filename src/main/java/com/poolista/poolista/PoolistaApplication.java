af7937c00e5d21fce9bcf925940b3a4cc6240e68
thaissan <thais.cavadas@hotmail.com>
2024-08-30 20:11:31 -0300

    Primeiro commit

---
+++ src/main/java/com/poolista/poolista/PoolistaApplication.java
@@ -0,0 +1,15 @@
+package com.poolista.poolista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolistaApplication.class, args);
+
+		
+	}
+
+}
---
+++ src/test/java/com/poolista/poolista/PoolistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poolista.poolista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
