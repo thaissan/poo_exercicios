a11799fd779d1225bc23fa70dce26242b4c0bd6e
thaissan <thais.cavadas@hotmail.com>
2024-09-06 14:47:17 -0300

    Exercicio 1

--- src/main/java/lista4/Exercicio1.java
+++ src/main/java/lista4/Exercicio1.java
@@ -1,16 +1,19 @@
 package lista4;
 
-import java.util.Scanner;
-import java.util.ArrayList;
-
 // 1) Voce é gerente de um supermercado e sabe que os valores das
 // vendas do dia são gravados em um vetor. Digite um código que faça o
 // fechamento (soma) de vendas do dia de maneira automatizada.
 
 public class Exercicio1 {
     public static void resolucao() {
-  
 
+        double [] vendasDiarias = {20.50, 3.50, 40.99, 73.60, 10.50};
+        double total = 0;
+
+        for (double vendas : vendasDiarias){
+            total += vendas;
+        }
+        System.out.printf("O fechamento total do dia foi de: R$%.2f.\n", total);
     }
 
 }
